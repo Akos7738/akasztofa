9798cb38a3e11f5949839e1579a6feb648f575be
Akos7738 <110132922+Akos7738@users.noreply.github.com>
2023-12-20 17:31:20 +0100

    Dáv volt.

--- game.py
+++ game.py
@@ -1,29 +1,35 @@
 import random
 
-def randomGen() -> str:
-    wordList = ["elkelkáposztásítottalanítottátok", "megszentségteleníthetetlenségeskedéseitekért", "alma", "körte", "barack", "szilva", "narancs", "eper", "citrom", "mandula", "datolya", "eperfa"]
-    return random.choice(wordList)
+def szar_vagy():
+    exit("Szar vagy!")
 
-wordTemplate = list()
+def randomGen() -> str:
+    wordList = ["elkelkáposztásítottalanítottátok", "megszentségteleníthetetlenségeskedéseitekért", "alma", "körte", "barack", "szilva", "narancs", "eper", "citrom", "mandula", "datolya", "eperfa", "tyúxar"]
+    return random.choice(wordList)
 
 word = randomGen()
 
-print(word)
-
+wordTemplate = list()
 for i in range(len(word)):
     wordTemplate.append("_")
-
-print()
-print(" ".join(wordTemplate))
-
+        
+print(word)
 print("\nTaláld ki a megadott szót! 10 rossz válasz után vége. (betűket várok.)")
-tip = input("Az ön tipje: ")
-
-if tip not in word:
-    print("Szar vagy!")
-else:
-    for i in range(len(word)):
-        if word[i] == tip:
-            wordTemplate[i] = tip
 
 print(" ".join(wordTemplate))
+
+while "".join(wordTemplate) != word:
+    print()
+    tip = input("Az ön tipje: ")
+    print()
+
+    if tip not in word:
+        szar_vagy()
+    else:
+        for i in range(len(word)):
+            if word[i] == tip:
+                wordTemplate[i] = tip
+
+    print(" ".join(wordTemplate))
+
+print(f"Ügyes vagy, a megfejtés: {word}")
