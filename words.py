f4f6416e80c2751159579ed96c523234966d09b1
czmorekakos <czmorekakos@gmail.com>
2024-01-25 12:23:01 +0100

    git config user.name czmorekakos git config user.email czmorekakos@gmail.com

--- words.py
+++ words.py
@@ -1,6 +1,23 @@
 import random
 
 def randomGen() -> str:
-    wordList = ["elkelkáposztásítottalanítottátok", "megszentségteleníthetetlenségeskedéseitekért", "eltöredezettségmentesítőtleníttethetetlenségtelenítőtlenkedhetnétek", "alma", "körte", "barack", "szilva", "narancs", "eper", "citrom", "mandula", "datolya", "eperfa"]
-    return random.choice(wordList)
+    rawText = list()
+    words = list()
+
+    with open("./kisherceg.txt", "r", encoding="utf-8") as text:
+        rawText = text.readlines()
+    
+#print(rawText)
+
+    textContent = list()
+    for line in rawText:
+        textContent.append(line.strip().replace("-","").replace(".","").replace(",","").replace("!","").replace("?","").replace(";","").replace(":","").replace("'","").replace('"','').replace("˝","").replace("´","").replace("”","").replace("(","").replace(")","").replace("„",""))
+    
+    for line in textContent:
+        tempLine = line.split(" ")
+        for w in line.split():
+            if len(w) > 3:
+                words.append(w)
+#print(words)
+    return(random.choice(words))
 
